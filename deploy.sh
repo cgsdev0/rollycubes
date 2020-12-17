39adc2ec2749c666afdd1b7a56dd7d21ecbf2692
Shane Schulte <shane.schulte@samsara.com>
2020-12-17 15:53:36 -0800

    epic release script 2.0

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-# build server
-make release && \
-    ssh root@shaneschulte.com systemctl stop dice_beta && \
-    scp GameServer root@shaneschulte.com:/usr/bin/GameServer_beta && \
-    ssh root@shaneschulte.com systemctl start dice_beta
+branch=$(git rev-parse --abbrev-ref HEAD)
 
-# build client
-cd client
-rm -rf build && \
-    yarn build && \
-    ssh root@shaneschulte.com sudo rm -rf /var/www/beta && \
-    scp -r build root@shaneschulte.com:/var/www/beta
+case "$branch" in
+    beta)
+    # build server
+    make release && \
+        ssh root@shaneschulte.com systemctl stop dice_beta && \
+        scp GameServer root@shaneschulte.com:/usr/bin/GameServer_beta && \
+        ssh root@shaneschulte.com systemctl start dice_beta
 
+    # build client
+    cd client
+    rm -rf build && \
+        yarn build && \
+        ssh root@shaneschulte.com sudo rm -rf /var/www/beta && \
+        scp -r build root@shaneschulte.com:/var/www/beta
+    ;;
+    master)
+    # build server
+    make release && \
+        ssh root@shaneschulte.com systemctl stop dice && \
+        scp GameServer root@shaneschulte.com:/usr/bin/GameServer && \
+        ssh root@shaneschulte.com systemctl start dice
 
-# make release && \
-#     ssh root@shaneschulte.com systemctl stop dice && \
-#     scp GameServer root@shaneschulte.com:/usr/bin/. && \
-#     ssh root@shaneschulte.com systemctl start dice
+    # build client
+    cd client
+    rm -rf build && \
+        yarn build && \
+        ssh root@shaneschulte.com sudo rm -rf /var/www/build && \
+        scp -r build root@shaneschulte.com:/var/www/.
+    ;;
+
+    *)
+    echo "Not a release-able branch!"
+    exit 1
+    ;;
+esac
