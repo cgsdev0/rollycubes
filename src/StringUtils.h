0abdf451eeb5ec83ff83b5bdd543fde095000c6c
Shane Schulte <schulte.shane@gmail.com>
2021-06-06 21:32:23 -0700

    Create StringUtils.h

---
+++ src/StringUtils.h
@@ -0,0 +1,16 @@
+#include <string>
+
+// Trims a string to a certain amount of characters (unicode aware).
+std::string trimString(const std::string& a, int len) {
+  std::string res;
+  res.reserve(len);
+  int count = 0;
+  for(char c : a) {
+    res += c;
+    if ((c & 0xC0) != 0x80) {
+      c++;
+    }
+    if (count >= len) break;
+  }
+  return res;
+}
