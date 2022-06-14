472aa0956514397a5d902fe2134e8c8965294032
Sarah Schulte <sarah.schulte@samsara.com>
2022-06-14 07:04:44 -0700

    they really did HOCs dirty

--- client/src/hocs/requires_session.tsx
+++ client/src/hocs/requires_session.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
-const RequiresSessionWrapper = (children: any) => {
+const RequiresSessionWrapper = (props: { children: any }) => {
   const location = useLocation();
   if (!document.cookie.includes("_session")) {
     return (
       <Navigate to="/" replace={true} state={{ redirect: location.pathname }} />
     );
   }
-  return children;
+  return <>{props.children}</>;
 };
 
 export const RequiresSession = <P extends object>(
