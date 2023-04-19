10b289459b567c8dd3d502bda5403428f959dd68
Sarah Schulte <git@sarah.engineer>
2023-04-18 23:50:41 -0700

    don't show tooltip for guests

--- client/src/ui/avatar.tsx
+++ client/src/ui/avatar.tsx
@@ -1,52 +1,54 @@
-import React from "react";
-import { css, styled } from "stitches.config";
-import defaultIcon from "../../public/default_player.png";
-import crownIcon from "../../public/crown.png";
-import { DisconnectedIcon } from "./icons/disconnected";
+import React from 'react'
+import { css, styled } from 'stitches.config'
+import defaultIcon from '../../public/default_player.png'
+import crownIcon from '../../public/crown.png'
+import { DisconnectedIcon } from './icons/disconnected'
 
 interface Props {
-  imageUrl?: string | null;
-  n?: number;
-  size?: number;
-  crown?: boolean;
-  disconnected?: boolean;
+  imageUrl?: string | null
+  n?: number
+  size?: number
+  crown?: boolean
+  disconnected?: boolean
+  isSignedIn?: boolean
 }
 
-const avatarWrapper = css({
-  alignItems: "center",
-  marginRight: 8,
-  position: "relative",
-  cursor: "pointer",
-  "& .avatar": {
-    borderRadius: "50%",
-  },
-});
+const avatarWrapper = (isSignedIn: boolean) =>
+  css({
+    alignItems: 'center',
+    marginRight: 8,
+    position: 'relative',
+    cursor: isSignedIn ? 'pointer' : 'inherit',
+    '& .avatar': {
+      borderRadius: '50%',
+    },
+  })()
 
 const disconnectedWrapper = css({
-  position: "absolute",
+  position: 'absolute',
   bottom: -5,
   zIndex: 5,
   left: -3,
-  transform: "scale(80%)",
-  backgroundColor: "#000000aa",
-  borderRadius: "50%",
-});
-const Crown = styled("img", {
-  position: "absolute",
+  transform: 'scale(80%)',
+  backgroundColor: '#000000aa',
+  borderRadius: '50%',
+})
+const Crown = styled('img', {
+  position: 'absolute',
   top: -6,
   zIndex: 5,
   left: 6,
-});
+})
 
 const Avatar = React.forwardRef<HTMLSpanElement, Props>((props, ref) => {
-  const { size, imageUrl, n, crown, disconnected } = props;
+  const { size, imageUrl, n, crown, disconnected, isSignedIn } = props
 
-  const forSureImageUrl = imageUrl || defaultIcon;
+  const forSureImageUrl = imageUrl || defaultIcon
 
-  const size2 = size || 36;
+  const size2 = size || 36
 
   return (
-    <span className={avatarWrapper()} ref={ref}>
+    <span className={avatarWrapper(Boolean(isSignedIn))} ref={ref}>
       <img
         className="avatar"
         alt="avatar"
@@ -63,7 +65,7 @@ const Avatar = React.forwardRef<HTMLSpanElement, Props>((props, ref) => {
         </span>
       ) : null}
     </span>
-  );
-});
+  )
+})
 
-export default Avatar;
+export default Avatar
--- client/src/ui/player.tsx
+++ client/src/ui/player.tsx
@@ -1,96 +1,104 @@
-import HorizontalScroll from "react-horizontal-scrolling";
-import { connect } from "react-redux";
-import { css } from "stitches.config";
-import { selectIsSpectator, selectSelfIndex, selectTurnIndex } from "../selectors/game_selectors";
-import { ReduxState } from "../store";
-import { Achievement, Player } from "../types/store_types";
-import Avatar from "./avatar";
-import { usePopperTooltip } from "react-popper-tooltip";
+import HorizontalScroll from 'react-horizontal-scrolling'
+import { connect } from 'react-redux'
+import { css } from 'stitches.config'
+import {
+  selectIsSpectator,
+  selectSelfIndex,
+  selectTurnIndex,
+} from '../selectors/game_selectors'
+import { ReduxState } from '../store'
+import { Achievement, Player } from '../types/store_types'
+import Avatar from './avatar'
+import { usePopperTooltip } from 'react-popper-tooltip'
 
 // TODO: remove
-import "react-popper-tooltip/dist/styles.css";
-import { KickIcon } from "./icons/kick";
-import React from "react";
+import 'react-popper-tooltip/dist/styles.css'
+import { KickIcon } from './icons/kick'
+import React from 'react'
 
 interface Props {
-  player: Player;
-  n: number;
-  self_index?: number;
-  turn_index: number;
-  socket?: WebSocket;
-  isSpectator: boolean;
+  player: Player
+  n: number
+  self_index?: number
+  turn_index: number
+  socket?: WebSocket
+  isSpectator: boolean
 }
 
+const ADJUST_FOR_STUPID_FONT = -5
+
 const playerName = css({
-  overflow: "hidden",
-  height: "100%",
-  width: "100%",
-  alignItems: "center",
-  display: "flex",
-});
+  overflow: 'hidden',
+  height: '100%',
+  width: '100%',
+  alignItems: 'center',
+  display: 'flex',
+  marginBottom: ADJUST_FOR_STUPID_FONT,
+})
 
 const playerRow = css({
-  display: "flex",
-  justifyContent: "space-between",
+  display: 'flex',
+  justifyContent: 'space-between',
   lineHeight: 0,
   minHeight: 36,
-  "&.highlight": {
-    backgroundColor: "$brandFaded",
+  '&.highlight': {
+    backgroundColor: '$brandFaded',
   },
   paddingLeft: 30,
   paddingRight: 30,
   paddingTop: 2,
   paddingBottom: 2,
-  position: "relative",
-});
+  position: 'relative',
+})
 
 const avatarAndName = css({
-  display: "flex",
-  maxWidth: "75%",
-  alignItems: "center",
-});
+  display: 'flex',
+  maxWidth: '75%',
+  alignItems: 'center',
+})
 
 const score = css({
-  display: "flex",
-  alignItems: "center",
-});
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: ADJUST_FOR_STUPID_FONT,
+})
 
 const kick = css({
-  display: "flex",
-  alignItems: "center",
-  cursor: "pointer",
-  height: "100%",
-  position: "absolute",
+  display: 'flex',
+  alignItems: 'center',
+  cursor: 'pointer',
+  height: '100%',
+  position: 'absolute',
   top: 0,
   left: 5,
-});
+})
 
 const PlayerComponent = (props: Props) => {
   const changeName = () => {
-    const e = window.prompt("Enter a name: ", props.player.name);
-    if (e === null) return;
+    const e = window.prompt('Enter a name: ', props.player.name)
+    if (e === null) return
     if (props.socket) {
-      props.socket.send(JSON.stringify({ type: "update_name", name: e }));
-      localStorage.setItem("name", e);
+      props.socket.send(JSON.stringify({ type: 'update_name', name: e }))
+      localStorage.setItem('name', e)
     }
-  };
+  }
 
   const onKick = () => {
-    const { player, n } = props;
-    const e = window.confirm(`Are you sure you want to kick ${player.name}?`);
+    const { player, n } = props
+    const e = window.confirm(`Are you sure you want to kick ${player.name}?`)
     if (e && props.socket) {
-      props.socket.send(JSON.stringify({ type: "kick", id: n }));
+      props.socket.send(JSON.stringify({ type: 'kick', id: n }))
     }
-  };
+  }
 
-  const [tooltipVisible, setTooltipVisible] = React.useState(false);
+  const [tooltipVisible, setTooltipVisible] = React.useState(false)
   const { getArrowProps, getTooltipProps, setTooltipRef, setTriggerRef } =
     usePopperTooltip({
       visible: tooltipVisible,
       onVisibleChange: setTooltipVisible,
       interactive: true,
-      trigger: "click",
-    });
+      trigger: 'click',
+    })
 
   const overridePosition = (
     { left, top }: { left: number; top: number },
@@ -99,27 +107,27 @@ const PlayerComponent = (props: Props) => {
 
     node: any
   ) => {
-    const d = document.documentElement;
-    left = Math.min(d.clientWidth - node.clientWidth, left);
-    top = Math.min(d.clientHeight - node.clientHeight, top);
-    left = Math.max(0, left);
-    top = Math.max(0, top);
-    return { top, left };
-  };
+    const d = document.documentElement
+    left = Math.min(d.clientWidth - node.clientWidth, left)
+    top = Math.min(d.clientHeight - node.clientHeight, top)
+    left = Math.max(0, left)
+    top = Math.max(0, top)
+    return { top, left }
+  }
 
-  const { n, player, self_index, turn_index } = props;
-  const imageUrl = player.userData?.image_url;
+  const { n, player, self_index, turn_index } = props
+  const imageUrl = player.userData?.image_url
 
-  const turnHighlight = turn_index === n ? " highlight" : "";
+  const turnHighlight = turn_index === n ? ' highlight' : ''
 
   return (
     <>
       {tooltipVisible && (
         <div
           ref={setTooltipRef}
-          {...getTooltipProps({ className: "tooltip-container" })}
+          {...getTooltipProps({ className: 'tooltip-container' })}
         >
-          <div {...getArrowProps({ className: "tooltip-arrow" })} />
+          <div {...getArrowProps({ className: 'tooltip-arrow' })} />
           <TooltipContents {...props} />
         </div>
       )}
@@ -131,7 +139,8 @@ const PlayerComponent = (props: Props) => {
       >
         <span className={avatarAndName()}>
           <Avatar
-            ref={setTriggerRef}
+            ref={player.userData ? setTriggerRef : undefined}
+            isSignedIn={Boolean(player.userData)}
             disconnected={!player.connected}
             imageUrl={imageUrl}
             n={n}
@@ -147,26 +156,26 @@ const PlayerComponent = (props: Props) => {
         )}
       </div>
     </>
-  );
-};
+  )
+}
 
 const TooltipContents: typeof PlayerComponent = (props) => {
-  const { n, player } = props;
-  const imageUrl = player.userData?.image_url;
-  const doubles = player.userData?.stats?.doubles || 0;
-  const wins = player.userData?.stats?.wins || 0;
-  const games = player.userData?.stats?.games || 0;
-  const rolls = player.userData?.stats?.rolls || 0;
+  const { n, player } = props
+  const imageUrl = player.userData?.image_url
+  const doubles = player.userData?.stats?.doubles || 0
+  const wins = player.userData?.stats?.wins || 0
+  const games = player.userData?.stats?.games || 0
+  const rolls = player.userData?.stats?.rolls || 0
 
-  const losses = games - wins;
+  const losses = games - wins
 
-  const win_rate = Math.floor((wins / (games || 1)) * 1000) / 10;
-  const doubles_rate = Math.floor((doubles / (rolls || 1)) * 1000) / 10;
+  const win_rate = Math.floor((wins / (games || 1)) * 1000) / 10
+  const doubles_rate = Math.floor((doubles / (rolls || 1)) * 1000) / 10
 
   const join_date = new Date(props.player.userData?.createdDate || 0)
     .toDateString()
-    .split(" ");
-  join_date.shift();
+    .split(' ')
+  join_date.shift()
 
   return React.useMemo(
     () => (
@@ -175,7 +184,7 @@ const TooltipContents: typeof PlayerComponent = (props) => {
           <Avatar imageUrl={imageUrl} size={32} />
           <span>{player.name || `User${n + 1}`}</span>
         </h1>
-        <p>Player since {join_date.join(" ")}</p>
+        <p>Player since {join_date.join(' ')}</p>
         <h2>Stats</h2>
         <p>
           Doubles: {doubles} / {rolls} rolls ({doubles_rate}%)
@@ -196,25 +205,25 @@ const TooltipContents: typeof PlayerComponent = (props) => {
       </>
     ),
     []
-  );
-};
+  )
+}
 
 const AchievementImg = (props: Achievement) => {
-  const [tooltipVisible, setTooltipVisible] = React.useState(false);
+  const [tooltipVisible, setTooltipVisible] = React.useState(false)
   const { getArrowProps, getTooltipProps, setTooltipRef, setTriggerRef } =
     usePopperTooltip({
       visible: tooltipVisible,
       onVisibleChange: setTooltipVisible,
-    });
+    })
   return React.useMemo(
     () => (
       <>
         {tooltipVisible && (
           <div
             ref={setTooltipRef}
-            {...getTooltipProps({ className: "tooltip-container" })}
+            {...getTooltipProps({ className: 'tooltip-container' })}
           >
-            <div {...getArrowProps({ className: "tooltip-arrow" })} />
+            <div {...getArrowProps({ className: 'tooltip-arrow' })} />
             <header>{props.achievement.name}</header>
             <p>{props.achievement.description}</p>
           </div>
@@ -224,13 +233,13 @@ const AchievementImg = (props: Achievement) => {
           width={48}
           height={48}
           alt={props.achievement.description}
-          src={props.achievement.image_url || "//via.placeholder.com/48"}
+          src={props.achievement.image_url || '//via.placeholder.com/48'}
         />
       </>
     ),
     [tooltipVisible, getTooltipProps]
-  );
-};
+  )
+}
 
 const mapStateToProps = (state: ReduxState) => {
   return {
@@ -238,7 +247,7 @@ const mapStateToProps = (state: ReduxState) => {
     turn_index: selectTurnIndex(state),
     socket: state.connection.socket,
     isSpectator: selectIsSpectator(state),
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps)(PlayerComponent);
+export default connect(mapStateToProps)(PlayerComponent)
