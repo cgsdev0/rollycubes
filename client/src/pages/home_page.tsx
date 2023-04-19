f44c6f78f6e9100f52235305a1ab196a16aa128e
Sarah Schulte <git@sarah.engineer>
2023-04-19 00:37:06 -0700

    beautiful logo

--- client/src/pages/home_page.tsx
+++ client/src/pages/home_page.tsx
@@ -1,82 +1,104 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { css } from "stitches.config";
-import { Button } from "../ui/buttons/button";
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { css, styled } from 'stitches.config'
+import { Button } from '../ui/buttons/button'
+import itsBoatsLogo from '../../public/itsboats.png'
 
 interface Game {
-  last_updated: number;
-  host_name: string;
-  code: string;
-  player_count: number;
+  last_updated: number
+  host_name: string
+  code: string
+  player_count: number
 }
+
+const StartButton = styled('button', {
+  backgroundColor: '#16B808',
+  flexBasis: 'initial',
+  color: 'white',
+  textTransform: 'uppercase',
+  borderRadius: 100,
+  height: 56,
+  letterSpacing: '0.16em',
+  fontSize: 16,
+  paddingTop: 2,
+  fontFamily: 'Amiko',
+  '&:hover': {
+    backgroundColor: '#26D828',
+  },
+  '&:active': {
+    backgroundColor: '#107804',
+  },
+})
 
 const content = css({
   width: 576,
-  display: "flex",
-  flexDirection: "column",
-});
+  display: 'flex',
+  flexDirection: 'column',
+})
 
 const hostNameCol = css({
-  textOverflow: "ellipsis",
-  overflow: "hidden",
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
   maxWidth: 210,
-});
+})
 const playerCountCol = css({
   width: 120,
-});
+})
 const tableWrapper = css({
-  overflowY: "auto",
-  display: "flex",
-});
+  overflowY: 'auto',
+  display: 'flex',
+})
 const gameInfoPanel = css({
   borderRadius: 16,
-  backgroundColor: "#151515",
-  display: "flex",
-  flexDirection: "column",
+  backgroundColor: '#151515',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-end',
+  gap: 115,
   width: 240,
   padding: 48,
-});
+})
 
 const HomePage = () => {
-  const [pressed, setPressed] = React.useState(false);
-  const [hasData, setHasData] = React.useState(false);
-  const [games, setGames] = React.useState<Game[]>([]);
+  const [pressed, setPressed] = React.useState(false)
+  const [hasData, setHasData] = React.useState(false)
+  const [games, setGames] = React.useState<Game[]>([])
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const fetchGameList = async () => {
     try {
-      const data = await window.fetch("/list");
-      const allGames = await data.json();
+      const data = await window.fetch('/list')
+      const allGames = await data.json()
       const games = allGames.rooms
         .filter((game: Game) => game.player_count)
         .sort((a: Game, b: Game) => {
           if (a.player_count === b.player_count) {
             // how should we break ties
-            return b.last_updated > a.last_updated;
+            return b.last_updated > a.last_updated
           }
           // move full lobbies to the bottom
           if (b.player_count === 8 && a.player_count !== 8) {
-            return -1;
+            return -1
           }
           // default: sort by player count
-          return b.player_count - a.player_count;
-        });
-      setGames(games);
-      setHasData(true);
+          return b.player_count - a.player_count
+        })
+      setGames(games)
+      setHasData(true)
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-  };
+  }
   const onStart = (priv: boolean) => async () => {
-    navigate(`/onboard`);
-  };
+    navigate(`/onboard`)
+  }
 
   React.useEffect(() => {
-    const interval = setInterval(fetchGameList, 2000);
-    fetchGameList();
-    return () => clearInterval(interval);
-  }, []);
+    const interval = setInterval(fetchGameList, 2000)
+    fetchGameList()
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <>
@@ -93,7 +115,7 @@ const HomePage = () => {
                 {games.map((game) => (
                   <tr key={game.code}>
                     <td className={hostNameCol()}>
-                      {game.host_name || "unknown"}
+                      {game.host_name || 'unknown'}
                     </td>
                     <td className={playerCountCol()}>
                       {game.player_count} / 8
@@ -110,16 +132,17 @@ const HomePage = () => {
             </table>
           </div>
         ) : (
-          <p>{!hasData ? "Loading..." : "No public lobbies found"}</p>
+          <p>{!hasData ? 'Loading...' : 'No public lobbies found'}</p>
         )}
       </div>
       <div className={gameInfoPanel()}>
-        <Button onClick={onStart(false)} disabled={pressed}>
-          Start Game
-        </Button>
+        <img src={itsBoatsLogo} />
+        <StartButton onClick={onStart(false)} disabled={pressed}>
+          New Game
+        </StartButton>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
