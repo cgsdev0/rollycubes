5b72113915fed36f5114ddd43cf14b09eb41404b
Sarah Schulte <git@sarah.engineer>
2023-07-28 03:52:39 -0700

    very slow day

--- ssh-gateway/main.go
+++ ssh-gateway/main.go
@@ -11,27 +11,47 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
-	"github.com/cgsdev0/rollycubes/ssh-gateway/api"
+	api "github.com/cgsdev0/rollycubes/ssh-gateway/api"
 	"github.com/cgsdev0/rollycubes/ssh-gateway/user"
+	"github.com/muesli/termenv"
 
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	ssh "github.com/charmbracelet/ssh"
 	wish "github.com/charmbracelet/wish"
-	bm "github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/bubbletea"
 	lm "github.com/charmbracelet/wish/logging"
 	gossh "golang.org/x/crypto/ssh"
 	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
 )
 
+type CommonModel struct {
+	ctx     ssh.Context
+	ws      *websocket.Conn
+	program *tea.Program
+}
+
 type LobbyScene struct {
-	list list.Model
+	Common *CommonModel
+	list   list.Model
 }
 
 type GameScene struct {
+	Common *CommonModel
+	State  *api.GameState
+	Width  int
+	Height int
+
+	viewport   viewport.Model
+	textInput  textinput.Model
 	roomCode   string
 	disconnect chan struct{}
 }
@@ -40,12 +60,23 @@ type SwitchSceneMsg struct {
 	scene tea.Model
 }
 
+type WebsocketMsg struct {
+	Msg  []byte
+	Type string
+}
+
+func reverse[S ~[]E, E any](s S) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
 func (m LobbyScene) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case tea.WindowSizeMsg:
-		m.list.SetHeight(msg.Height - 2)
-		m.list.SetWidth(msg.Width - 4)
+		m.list.SetHeight(msg.Height)
+		m.list.SetWidth(msg.Width)
 
 	// Is it a key press?
 	case tea.KeyMsg:
@@ -83,7 +114,72 @@ func (m LobbyScene) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m GameScene) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case WebsocketMsg:
+		switch msg.Type {
+		case "welcome":
+			welcome, err := api.UnmarshalWelcomeMsg(msg.Msg)
+			if err != nil {
+				log.Printf("idfk it broke %v+\n", err)
+				break
+			}
+			state, err := api.UnmarshalGameState(msg.Msg)
+			if err != nil {
+				log.Printf("idfk it broke %v+\n", err)
+				break
+			}
+			fmt.Printf("Your index is %d\n", welcome.ID)
+			reverse(state.ChatLog)
+			m.State = &state
+			m.viewport.SetContent(strings.Join(m.State.ChatLog, "\n"))
+			m.viewport.SetYOffset(m.viewport.YOffset + 100000000) // lol
+		case "chat":
+			chat, err := api.UnmarshalChatMsg(msg.Msg)
+			if err != nil {
+				log.Printf("idfk it broke %v+\n", err)
+				break
+			}
+			m.State.ChatLog = append(m.State.ChatLog, chat.Msg)
+			m.viewport.SetContent(strings.Join(m.State.ChatLog, "\n"))
+			m.viewport.SetYOffset(m.viewport.YOffset + 100000000) // lol
+		}
+	case tea.WindowSizeMsg:
+		m.Height = msg.Height
+		m.Width = msg.Width
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			m.textInput.Blur()
+		case "i":
+			if m.textInput.Focused() {
+				break
+			}
+			cmd = m.textInput.Focus()
+			return m, cmd
+		case "enter":
+			if m.textInput.Focused() && m.textInput.Value() != "" {
+				val := m.textInput.Value()
+				if m.Common.ws != nil {
+					msg := api.ChatMsg{Type: api.Chat, Msg: val}
+					err := wsjson.Write(m.Common.ctx, m.Common.ws, msg)
+					if err != nil {
+						log.Println(err)
+						break
+					}
+				}
+				m.textInput.SetValue("")
+				return m, nil
+			}
+		}
+	}
+	var cmd2 tea.Cmd
+	m.textInput, cmd2 = m.textInput.Update(msg)
+	var cmd3 tea.Cmd
+	if !m.textInput.Focused() {
+		m.viewport, cmd3 = m.viewport.Update(msg)
+	}
+	return m, tea.Batch(cmd, cmd2, cmd3)
 }
 
 func (m GameScene) Init() tea.Cmd {
@@ -95,7 +191,18 @@ func (m LobbyScene) Init() tea.Cmd {
 }
 
 func (m GameScene) View() string {
-	return m.roomCode
+	// var chat = lipgloss.NewStyle().
+	// 	BorderStyle(lipgloss.RoundedBorder()).
+	// 	BorderForeground(lipgloss.Color("228")).
+	// 	BorderBackground(lipgloss.Color("63"))
+
+	// var chatStr string
+	// if m.State != nil {
+	// 	chatStr = strings.Join(m.State.ChatLog, "\n")
+	// }
+	return lipgloss.JoinVertical(lipgloss.Left,
+		m.viewport.View(),
+		m.textInput.View())
 }
 
 func (m LobbyScene) View() string {
@@ -103,16 +210,13 @@ func (m LobbyScene) View() string {
 }
 
 type model struct {
-	user *user.User
+	Common *CommonModel
+	user   *user.User
 
 	session ssh.Session
 
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
-
-	width  int
-	height int
+	Width  int
+	Height int
 
 	stuff    string
 	roomList api.RoomList
@@ -121,19 +225,22 @@ type model struct {
 	scene tea.Model
 }
 
-func startWebsocket(ctx ssh.Context, m model, s GameScene) {
+func startWebsocket(m model, s GameScene) {
+	ctx := m.Common.ctx
 	serverURL := "wss://rollycubes.com/ws/room/" + s.roomCode
 	c, _, err := websocket.Dial(ctx, serverURL, &websocket.DialOptions{HTTPHeader: http.Header{"Cookie": {"_session=awefo34irj2r04"}}})
 	if err != nil {
 		// ...
 		panic(err)
 	}
+	m.Common.ws = c
 	// Create a channel to signal when to close the connection
 
 	waitForClose := func(ctx context.Context, conn *websocket.Conn, done chan struct{}) {
 		defer close(done)
 		<-done
 		log.Println("recv")
+		m.Common.ws = nil
 		conn.Close(websocket.StatusInternalError, "goodbye")
 	}
 	readMessages := func(ctx context.Context, conn *websocket.Conn) {
@@ -145,6 +252,16 @@ func startWebsocket(ctx ssh.Context, m model, s GameScene) {
 				return
 			}
 			fmt.Printf("Received message: %s\t%s\n", message, messageType)
+			var wsMsg WebsocketMsg
+			var tmp struct {
+				Type string `json:"type"`
+			}
+			if err := json.Unmarshal(message, &tmp); err != nil {
+				log.Printf("Error unmarshaling message: %v", err)
+			}
+			wsMsg.Type = tmp.Type
+			wsMsg.Msg = message
+			m.Common.program.Send(wsMsg)
 		}
 	}
 	// Start a goroutine to read messages from the server
@@ -159,8 +276,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
+		m.Width = msg.Width
+		m.Height = msg.Height
 
 	// Is it a key press?
 	case tea.KeyMsg:
@@ -169,7 +286,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 
 		// These keys should exit the program.
-		case "ctrl+c", "q":
+		case "ctrl+q":
 			return m, tea.Quit
 
 			// The "enter" key and the spacebar (a literal space) toggle
@@ -177,8 +294,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "enter":
 			lobbyScene, ok := m.scene.(LobbyScene)
 			if !ok {
-				log.Println("not lobby")
-				return m, nil
+				break
 			}
 			if lobbyScene.list.SettingFilter() {
 				log.Println("filtering")
@@ -190,11 +306,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, nil
 			}
 			ch := make(chan struct{})
-			newScene := GameScene{roomCode: room.Code, disconnect: ch}
+			newScene := GameScene{
+				viewport:   viewport.New(m.Width, m.Height-1),
+				Common:     m.Common,
+				roomCode:   room.Code,
+				disconnect: ch,
+				Width:      m.Width,
+				Height:     m.Height,
+				textInput:  textinput.New(),
+			}
 			log.Println("new scene")
 			m.scene = newScene
-			go startWebsocket(m.session.Context(), m, newScene)
-			return m, nil
+			go startWebsocket(m, newScene)
 		}
 	}
 
@@ -212,6 +335,50 @@ func (m model) View() string {
 	}
 
 	return m.scene.View()
+}
+
+func bubbleTeaMiddleware(store *user.Store) wish.Middleware {
+	cp := termenv.ANSI256
+	h := func(s ssh.Session) *tea.Program {
+		pty, _, active := s.Pty()
+		if !active {
+			log.Printf("no active terminal, skipping")
+			return nil
+		}
+		key := s.PublicKey()
+		var u *user.User
+		if key != nil {
+			bytes := key.Marshal()
+			based := url.QueryEscape(b64.StdEncoding.EncodeToString(bytes))
+			u = store.Take(based)
+		}
+		commonModel := &CommonModel{ctx: s.Context()}
+		lobbyScene := LobbyScene{
+			Common: commonModel,
+			list:   list.New([]list.Item{}, list.NewDefaultDelegate(), pty.Window.Width-6, pty.Window.Height-2),
+		}
+		lobbyScene.list.Title = "Active Rooms"
+
+		model := &model{
+			Common: commonModel,
+
+			user: u,
+
+			session: s,
+			// A map which indicates which choices are selected. We're using
+			// the  map like a mathematical set. The keys refer to the indexes
+			// of the `choices` slice, above.
+			Width:  pty.Window.Width,
+			Height: pty.Window.Height,
+			scene:  lobbyScene,
+		}
+		opts := []tea.ProgramOption{tea.WithAltScreen()}
+		opts = append(opts, tea.WithInput(s), tea.WithOutput(s))
+		p := tea.NewProgram(model, opts...)
+		commonModel.program = p
+		return p
+	}
+	return bubbletea.MiddlewareWithProgramHandler(h, cp)
 }
 
 func main() {
@@ -247,37 +414,7 @@ func main() {
 			return true
 		}),
 		wish.WithMiddleware(
-			bm.Middleware(func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
-				pty, _, active := s.Pty()
-				if !active {
-					log.Printf("no active terminal, skipping")
-					return nil, nil
-				}
-				key := s.PublicKey()
-				var u *user.User
-				if key != nil {
-					bytes := key.Marshal()
-					based := url.QueryEscape(b64.StdEncoding.EncodeToString(bytes))
-					u = store.Take(based)
-				}
-				lobbyScene := LobbyScene{
-					list: list.New([]list.Item{}, list.NewDefaultDelegate(), pty.Window.Width-6, pty.Window.Height-2),
-				}
-				lobbyScene.list.Title = "Active Rooms"
-				model := &model{
-					user: u,
-
-					session: s,
-					// A map which indicates which choices are selected. We're using
-					// the  map like a mathematical set. The keys refer to the indexes
-					// of the `choices` slice, above.
-					selected: make(map[int]struct{}),
-					width:    pty.Window.Width,
-					height:   pty.Window.Height,
-					scene:    lobbyScene,
-				}
-				return model, []tea.ProgramOption{tea.WithAltScreen()}
-			}),
+			bubbleTeaMiddleware(&store),
 			lm.Middleware(),
 		))
 	if err != nil {
