4e444ff4e5728b1f0d0818d3c1ee5e78be7212a2
Sarah Schulte <git@sarah.engineer>
2024-01-26 11:27:40 -0800

    too subtle

--- client/src/ui/player.tsx
+++ client/src/ui/player.tsx
@@ -327,7 +327,7 @@ const AImg = styled('img', {
 
 const AchievementPlaceholder = styled('div', {
   borderRadius: 4,
-  border: '1px dashed $gray800',
+  border: '1px dashed $gray900',
   width: 48,
   height: 48,
 });
