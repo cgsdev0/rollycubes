5d366911325cd837ffd89b54519d71c65db825bf
Sarah Schulte <git@sarah.engineer>
2024-01-28 05:27:31 -0800

    no more proxying dumb stuff

--- client/functions/create.ts
+++
@@ -1,8 +0,0 @@
-export const onRequest: PagesFunction<{}> = async (context) => {
-  const request = context.request;
-    const url = new URL(request.url);
-    return await fetch(
-      url.protocol + '//prod.rollycubes.com' + url.pathname + (url.search || ''),
-      request
-    );
-};
