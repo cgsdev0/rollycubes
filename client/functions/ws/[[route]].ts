ae8a154dacc17dc5ab5820737dfc9d092a6ecc9a
Sarah Schulte <git@sarah.engineer>
2024-01-28 03:27:01 -0800

    add a log

--- client/functions/ws/[[route]].ts
+++ client/functions/ws/[[route]].ts
@@ -3,6 +3,9 @@ export const onRequest: PagesFunction<{}> = async (context) => {
   const upgradeHeader = request.headers.get('Upgrade');
   if (upgradeHeader || upgradeHeader === 'websocket') {
     const url = new URL(request.url);
+    console.log(
+      'wss://beta.rollycubes.com' + url.pathname + (url.search || '')
+    );
     return await fetch(
       'wss://beta.rollycubes.com' + url.pathname + (url.search || ''),
       request
