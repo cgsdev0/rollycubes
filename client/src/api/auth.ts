cc42fdb48fe0225b5f449aba4defa9103f8d03a3
Sarah Schulte <git@sarah.engineer>
2024-02-08 04:30:43 -0800

    bump histogram in optimistic update

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -7,7 +7,7 @@ import {
   fetchBaseQuery,
   FetchBaseQueryError,
 } from '@reduxjs/toolkit/query/react';
-import { ReduxState } from 'store';
+import { ReduxState, TARGET_SCORES } from 'store';
 import {
   AchievementData,
   AchievementUnlock,
@@ -206,6 +206,12 @@ export const optimisticUpdates = (
               win_hist: [0, 0, 0, 0, 0, 0],
             };
           }
+          u.stats!.win_hist[
+            TARGET_SCORES.findIndex(
+              (score) =>
+                score === state.game.players[state.game.turn_index].score
+            )
+          ]++;
           u.stats!.games += 1;
           if (data.id == i) {
             u.stats!.wins += 1;
