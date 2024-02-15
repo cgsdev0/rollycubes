787aefe9e50d048c88143ae7d2a406ae5ed48a3d
Jeff Irwin <jirwin505@gmail.com>
2024-02-14 23:24:01 -0500

    add a draft of an extremely rough fortran random number server

---
+++ random/docker-build.sh
@@ -0,0 +1,7 @@
+#/shebang
+
+# the only reason that this script exists is because jeff can't remember docker
+# commands
+
+sudo docker build . -t random
+
---
+++ random/docker-run.sh
@@ -0,0 +1,8 @@
+#/shebang
+
+# the only reason that this script exists is because jeff can't remember docker
+# commands
+
+#sudo docker build . -t random
+sudo docker run random
+
