afb5a9e5653489eb2b5f600890e2ded353774b0c
Jeff Irwin <jirwin505@gmail.com>
2024-02-16 22:52:16 -0500

    add rand_int function; cleanup

--- random/src/RandomServer.f90
+++ random/src/RandomServer.f90
@@ -1,21 +1,34 @@
 module random_m
 
-      use iso_c_binding
-      use mod_dill
+    use iso_c_binding
+    use mod_dill
 
-      implicit none
+    implicit none
 
-      character, parameter :: nullc = c_null_char
+    character, parameter :: nullc = c_null_char
 
 contains
 
 !===============================================================================
 
 function len_sz(str_)
-      character(len = *), intent(in) :: str_
-      integer(c_size_t) :: len_sz
-      len_sz = len(str_)
+    character(len = *), intent(in) :: str_
+    integer(c_size_t) :: len_sz
+    len_sz = len(str_)
 end function len_sz
+
+!===============================================================================
+
+integer function rand_int(min_, max_)
+    integer, intent(in) :: min_, max_
+
+    double precision :: double_
+
+    ! TODO roll our own rng just for lulz
+    call random_number(double_)
+    rand_int = int(double_ * (max_ - min_ + 1)) + min_
+
+end function rand_int
 
 !===============================================================================
 
@@ -28,23 +41,22 @@ program random_server
     use random_m
     implicit none
 
-    integer(c_int) :: connection, rc, socket
-    type(ipaddr) :: addr, addr_remote
+    character :: char_
     character(kind=c_char, len=IPADDR_MAXSTRLEN) :: address_string = ""
     character(len=*), parameter :: TCP_SUFFIX = c_carriage_return//c_new_line
 
+    integer, parameter :: num_sides = 6
+    integer, parameter :: base = num_sides  ! byte-packing encoding base
+    integer(c_int), parameter :: port = 5555
+
+    integer :: num, rolls(2)
+    integer(c_int) :: connection, rc, socket
     integer(c_int64_t) :: NO_DEADLINE = -1
 
-    character :: buffer*32, char_
-    character(len = :), allocatable :: str_
-    double precision :: dnum
-
-    integer, parameter :: num_sides = 6
-    integer, parameter :: base = num_sides
-    integer :: num, rolls(2)
+    type(ipaddr) :: addr, addr_remote
 
     ! 127.0.0.1 is not accessible outside of docker, but 0.0.0.0 is
-    rc = ipaddr_local(addr, "0.0.0.0"//nullc, 5555_c_int, IPADDR_IPV4)
+    rc = ipaddr_local(addr, "0.0.0.0"//nullc, port, IPADDR_IPV4)
 
     call ipaddr_str(addr, address_string)
 
@@ -59,25 +71,16 @@ program random_server
         call ipaddr_str(addr, address_string)
         write(*,*) "New connection from "//trim(address_string)
 
-        ! Subtract 1 from len to not count the null terminator
         connection = suffix_attach(connection, TCP_SUFFIX//nullc, &
             len_sz(TCP_SUFFIX))
 
-        ! TODO roll our own rng just for lulz
-        call random_number(dnum)
-        rolls(1) = int(dnum * num_sides) + 1
-        call random_number(dnum)
-        rolls(2) = int(dnum * num_sides) + 1
+        rolls(1) = rand_int(1, num_sides)
+        rolls(2) = rand_int(1, num_sides)
 
-        ! Pack two [1,6] rolls into a single byte, encoded as base-6
+        ! Pack two [1, 6] rolls into a single byte, encoded as base-6
         num = base * (rolls(1) - 1) + (rolls(2) - 1)
-        print *, "rolls = ", rolls
-        !print *, "num = ", num
-        print *, "sum = ", sum(rolls)
-
-        !write(buffer, "(i0)") num
-        !str_ = trim(buffer)
-        !rc = msend(connection, str_//nullc, len_sz(str_), NO_DEADLINE)
+        print "(a,i0,a,i0)", "rolls = ", rolls(1), " ", rolls(2)
+        print "(a,i0)"     , "sum   = ", sum(rolls)
 
         char_ = achar(num)
         rc = msend(connection, char_//nullc, len_sz(char_), NO_DEADLINE)
